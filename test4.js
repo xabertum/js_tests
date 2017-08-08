d1dedfe199974c776fee9c86e73872424b57ad07
xabertum <mohatar@gmail.com>
2017-08-08 12:36:28 +0200

    Minor changes

--- test4.js
+++ test4.js
@@ -2,12 +2,12 @@ var respuesta = prompt("Primer presidente de la democracia española:");
 
 do {
 
-    if (respuesta == "adolfo suarez") {
-        alert("Enhorabuena");
-    } else if (respuesta == "adolfo") {
+    if (respuesta == "adolfo") {
         var respuesta = prompt("Te falta el apellido. Intentelo de nuevo");
     } else if (respuesta == "suarez") {
         var respuesta = prompt("Te falta el nombre. Intentelo de nuevo");
     }
 
-} while (respuesta == "adolfo suarez" || respuesta == "adolfo" || respuesta == "suarez");
+} while (respuesta != "adolfo suarez" || respuesta == "adolfo" || respuesta == "suarez");
+
+alert("Enhorabuena");
