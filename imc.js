f0bd4a385a5b5eef00ccf56c144de011b4e51669
xabertum <mohatar@gmail.com>
2017-08-09 11:50:09 +0200

    Minor Changes

--- imc.js
+++ imc.js
@@ -1,6 +1,10 @@
+var peso = document.forms[0].peso.value;
+var altura = document.forms[0].altura.value;
+
+var imc = (peso / Math.pow(altura, 2)).toFixed(2);
+
 function calcular() {
-    var peso = document.forms[0].peso.value;
-    var altura = document.forms[0].altura.value;
+
 
     if (peso.length == 0) {
         alert("El peso no puede estar vacio");
@@ -8,11 +12,10 @@ function calcular() {
         alert("La altura no puede estar vacia");
     } else {
 
-
-
-
-
-
+        if (imc > 16) {
+            alert(imc);
+            document.getElementById("1").style.color = "red";
+        }
 
     }
 
