bf4e79a20b2e339b5678e82d7549f891d5d276cc
xabertum <mohatar@gmail.com>
2017-08-09 11:16:33 +0200

    js file changes

--- imc.js
+++ imc.js
@@ -1,2 +1,19 @@
+function calcular() {
+    var peso = document.forms[0].peso.value;
+    var altura = document.forms[0].altura.value;
+
+    if (peso.length == 0) {
+        alert("El peso no puede estar vacio");
+    } else if (altura.length == 0) {
+        alert("La altura no puede estar vacia");
+    } else {
 
 
+
+
+
+
+
+    }
+
+}
