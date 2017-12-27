7d07e0e451f277075ea69dfff8a898bdea7a786e
xabertum <mohatar@gmail.com>
2017-12-27 18:35:49 +0100

    Added test file

---
+++ switch_test.js
@@ -0,0 +1,18 @@
+
+var monthNum = prompt("Enter a month number: ");
+var monthName;
+
+switch (monthNum) {
+    case "1":
+        monthName = "January";
+        break;
+    case "2":
+        monthName = "February";
+        break;
+    default:
+        alert("Bad input");
+        
+};
+
+alert (monthName);
+
