a502a56c3324a2423e8b450d04252caa6bb1480c
xabertum <mohatar@gmail.com>
2017-12-28 16:14:42 +0100

    Added File

---
+++ Ejercicios_UOC/Ejercicio9.js
@@ -0,0 +1,10 @@
+var sFrase = prompt("introduce una frase...");
+
+function count(main_str, sub_str) {
+    if (sub_str.length <= 0) return main_str.length + 1;
+
+    subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    return (main_str.match(new RegExp(subStr, 'gi')) || []).length;
+}
+
+document.write(count(sFrase, 'a'));
