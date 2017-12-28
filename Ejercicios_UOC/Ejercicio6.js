d5670d5e7d4066f8898a90a955689db0f2120a39
xabertum <mohatar@gmail.com>
2017-12-28 15:32:02 +0100

    Added File

---
+++ Ejercicios_UOC/Ejercicio6.js
@@ -0,0 +1,5 @@
+var sNumber = parseInt(prompt("Introduce un numero: "));
+var sNumber2 = parseInt(prompt("Introduce otro numero: "));
+
+if (sNumber > sNumber2) document.write("El mayor es: " + sNumber);
+else document.write("El mayor es: " + sNumber2);
