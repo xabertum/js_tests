a5bdff072766cca12018f39af048ee8673a5f9d7
xabertum <mohatar@gmail.com>
2017-12-28 20:00:57 +0100

    Minor changes

--- Ejercicios_UOC/Ejercicio9.1.js
+++ Ejercicios_UOC/Ejercicio9.1.js
@@ -4,6 +4,7 @@ var sChar = prompt("Caracter a buscar: ");
 function count(main_str, sub_str) {
 
     return main_str.match(new RegExp(sub_str, 'gi')).length;
-}
+
+};
 
 document.write(count(sFrase, sChar));
