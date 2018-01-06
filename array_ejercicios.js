82f3d3769119ae743f03e14178fa4352b6816271
xabertum <mohatar@gmail.com>
2018-01-06 12:36:32 +0100

    Added new file

---
+++ array_ejercicios.js
@@ -0,0 +1,18 @@
+
+var oArray = ['España', 'Irlanda', 'EE.UU', 'Alemania', 'Francia', 'Portugal'];
+
+function onLength (arr) {
+    return arr.length;
+}
+
+function OnWrite (arr) {
+    return oArray;
+}
+
+
+document.write('Longitud del array' + onLength(oArray));
+document.write('Contenido del array' + OnWrite(oArray));
+
+
+
+
